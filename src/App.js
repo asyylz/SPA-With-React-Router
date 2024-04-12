2d2207a4ee5bf25f344196691d75946ddb703d5e
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-12 23:28:39 +0100

    Link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  createRoutesFromElements,
-  Route,
+  //createRoutesFromElements,
+  //Route,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />,
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />,
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+//const router = createBrowserRouter(routeDefinitions);
+
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
+// Link prevents the browser default of sending a HTTP request if the link is clicked.
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p> Go to
+      <Link to="/products">the list of products</Link> 
+      </p>
+    </>
+  );
 }
