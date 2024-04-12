bce2e9e06e3a280d81ffb97ec8df00ce684b9527
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-12 23:23:02 +0100

    old version define route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> },]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />,
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+);
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 function App() {
   return <RouterProvider router={router} />;
 }
