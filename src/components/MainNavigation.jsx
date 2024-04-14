0b400477005f3d69c649b96499ce6d1f8db8b62d
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-14 13:28:22 +0100

    NavLink

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,6 +1,10 @@
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
-import classes from './MainNavigation.module.css';
+// NavLink has one special behavior. If you add the class name prop to it, it's actually not the regular class name prop, which takes a string, but instead it's a prop that takes a function.
+
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
@@ -8,10 +12,26 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end 
+              // This end property indicates that this link should only be considered active if the currently active route ends with this path after the URL.
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
