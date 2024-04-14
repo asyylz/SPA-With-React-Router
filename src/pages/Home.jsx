093a4e7f128a19c3c1ba6ec0be5668813b91bfdf
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-14 13:33:42 +0100

    useNavigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
 // Link prevents the browser default of sending a HTTP request if the link is clicked.
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>Home Page</h1>
-      <p> Go to
-      <Link to="/products">the list of products</Link> 
+      <p>
+        {" "}
+        Go to
+        <Link to="/products">the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
