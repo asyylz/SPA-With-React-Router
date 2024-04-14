8a588259243ef565c5093b38498b6bb8d130c770
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-14 13:11:42 +0100

    error page

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
-import classes from "./Root.module.css";
 import MainNavigation from "../components/MainNavigation";
 export default function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <main className={classes.content}>
+      <main>
         <Outlet />
       </main>
     </>
