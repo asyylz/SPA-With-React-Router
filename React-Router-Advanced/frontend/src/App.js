f255422d68d8894964c755f2aa02dd12f366f5d4
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-14 20:18:23 +0100

    eventitem loader

--- React-Router-Advanced/frontend/src/App.js
+++ React-Router-Advanced/frontend/src/App.js
@@ -23,7 +23,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -46,7 +48,11 @@ function App() {
               element: <EventsPage />,
               loader: eventsLoader,
             },
-            { path: ":id", element: <EventDetailPage /> },
+            {
+              path: ":id",
+              element: <EventDetailPage />,
+              loader: eventDetailLoader,
+            },
             { path: "new", element: <NewEventPage /> },
             { path: ":id/edit", element: <EditEventPage /> },
           ],
--- React-Router-Advanced/frontend/src/pages/EventDetail.jsx
+++ React-Router-Advanced/frontend/src/pages/EventDetail.jsx
@@ -1,11 +1,28 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, json, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 export default function EventDetailPage() {
   const params = useParams();
+
+  const data = useLoaderData();
   return (
     <>
-      <h1>Event Detail Page</h1>
-      <p>Event Id:{params.id}</p>
-      <Link to="edit"> Edit Event</Link>
+      <EventItem event={data.event} />
     </>
   );
+}
+
+export async function loader({ request, params }) {
+  const id = params.id;
+
+  const response = await fetch("http://localhost:8080/events" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Could not load fetch details for selected event" },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
 }
