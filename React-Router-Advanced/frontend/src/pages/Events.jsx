7058f8f113a2d9c619cadfb708788e26ca556129
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-14 17:29:31 +0100

    outsorurcing fetching function

--- React-Router-Advanced/frontend/src/pages/Events.jsx
+++ React-Router-Advanced/frontend/src/pages/Events.jsx
@@ -1,30 +1,26 @@
-import { useEffect, useState } from 'react';
-
-import EventsList from '../components/EventsList';
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-   
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
+  const events = useLoaderData();
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList events={events} />
     </>
   );
 }
 
 export default EventsPage;
+
+/* ------------ outsourcing fetching function ----------- */
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
