a2731987543a335080c3bb0fb3cc2a69a5e7f606
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-16 10:25:06 +0100

    update

--- React-Router-Advanced/frontend/src/pages/EditEvent.jsx
+++ React-Router-Advanced/frontend/src/pages/EditEvent.jsx
@@ -2,6 +2,6 @@ import EventForm from "../components/EventForm";
 import { useRouteLoaderData } from "react-router-dom";
 export default function EditEventPage() {
   const data = useRouteLoaderData("event-detail");
- 
+
   return <EventForm method="patch" event={data.event} />;
 }
