21c3a8972ab3b600a06dc08dddee8e7c68e899bc
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-16 10:11:25 +0100

    merging edit and new event  calls

--- React-Router-Advanced/frontend/src/components/EventForm.jsx
+++ React-Router-Advanced/frontend/src/components/EventForm.jsx
@@ -4,6 +4,7 @@ import {
   useNavigation,
   useActionData,
 } from "react-router-dom";
+import { json, redirect } from "react-router";
 
 import classes from "./EventForm.module.css";
 
@@ -17,7 +18,7 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method="post" className={classes.form}>
+    <Form method={method} className={classes.form}>
       {data && data.errors && (
         <ul>
           {Object.values(data.errors).map((err) => (
@@ -78,3 +79,37 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  let url = "http://localhost:8080/events";
+
+  if (method === "patch") {
+    const id = params.id;
+    url = "http://localhost:8080/events/" + id;
+  }
+  const response = await fetch(url, {
+    method: { method },
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event" }, { status: 500 });
+  }
+  return redirect("/events");
+}
