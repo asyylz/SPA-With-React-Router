5a6ad92deb907949eb8ab70e01f6feb83a3cab76
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-16 11:26:35 +0100

    defer for event detail page with eventlist

--- React-Router-Advanced/frontend/src/pages/EventDetail.jsx
+++ React-Router-Advanced/frontend/src/pages/EventDetail.jsx
@@ -1,15 +1,31 @@
-import { json, useRouteLoaderData, redirect } from "react-router-dom";
+import {
+  json,
+  useRouteLoaderData,
+  redirect,
+  defer,
+  Await,
+} from "react-router-dom";
+import { Suspense } from "react";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 export default function EventDetailPage() {
-  const data = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
   return (
     <>
-      <EventItem event={data.event} />
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 }
-
-export async function loader({ request, params }) {
+async function loadEvent(id) {
   const id = params.id;
 
   const response = await fetch("http://localhost:8080/events/" + id);
@@ -21,14 +37,38 @@ export async function loader({ request, params }) {
       }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //return { isError: true, message: "Couldn't load events" };
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    return json({ message: "Could not fetch events." }, { status: 500 });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.id;
+  return defer({
+    event: loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ params, request }) {
   const id = params.id;
   const response = await fetch("http://localhost:8080/events/" + id, {
-    method:request.method,
+    method: request.method,
   });
 
   if (!response.ok) {
